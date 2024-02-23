382299e693177345f427b40839d8336391a29bea
sreelal-s-devops <161043740+sreelal-s-devops@users.noreply.github.com>
2024-02-23 22:07:48 +0530

    sample

---
+++ object.js
@@ -0,0 +1,13 @@
+let person = {
+    name :"sreelal s",
+    age  : 25,
+    qualification: "Bachelor in computer application",
+    hobbies:["reading","gaming","paper crafting"],
+    address:{
+        line1:"sreela bhavan",
+        line2:"pangod"
+
+    }
+    
+}
+console.log(person.address.line1)
