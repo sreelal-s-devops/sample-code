fb48dd37ea202120ce58578a185833b353343d2b
sreelal-s-devops <161043740+sreelal-s-devops@users.noreply.github.com>
2024-02-23 19:56:37 +0530

    My First Commit
    first commit to git

---
+++ typeConversion.js
@@ -0,0 +1,7 @@
+let a = 1
+let b ="2"
+console.log(a+b)// output 12 ,as b is a string
+//----------------------------------------------------------------------
+console.log(parseInt(b)+a)
+// output is 3 , as we converted 'b' to number
+//-----------------------------------------------------------------------
